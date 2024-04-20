ee416dfd2921eb92f0bb47d702e689ef914692c0
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-04-20 18:27:02 +0900

    solved

---
+++ alg1181.py
@@ -0,0 +1,14 @@
+N = int(input())
+sets = [set() for _ in range(52)]
+ls = [list() for _ in range(52)]
+for i in range(N):
+    word = input()
+    sets[len(word)].add(word)
+    
+
+for i in range(51):
+    if len(sets[i]) != 0:
+        ls[i] = sorted(list(sets[i]))
+        for x in ls[i]:
+            print(x)
+
